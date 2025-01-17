c3d2430a1cc71622ee48d3a968c6a7e93bdb0d7b
Kuui22 <91069623+Kuui22@users.noreply.github.com>
2025-01-17 16:08:24 +0100

    crt turning on

--- js/index.js
+++ js/index.js
@@ -1,14 +1,22 @@
 
 $("document").ready(function () {
+  const crtScreen = document.getElementById("crt-screen");
+  const content = document.getElementById("content");
+
+  setTimeout(() => {
+    crtScreen.style.display = "none";
+    content.style.display = "block";
+  }, 1500); // Matches the duration of the crt-glow animation
+
 var clients = document.getElementById('clients');
 var services = document.getElementById('services');
 
-clients.addEventListener('click', function() {
+clients.addEventListener('click', function () {
   $(clients).toggleClass("active");
   $(".parent:not(#clients)").toggleClass("invisible");
 }, false);
 
-services.addEventListener('click', function() {
+services.addEventListener('click', function () {
   $(services).toggleClass("active");
   $(".parent:not(#services)").toggleClass("invisible");
 }, false);
